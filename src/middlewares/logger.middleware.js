ae4a63dce02e64892b694f98ad29c3091011ef93
Kira <kira.petit@optivamedia.com>
2020-03-15 20:43:45 +0100

    remove log from logger middleware

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = (req, res, next) => {
-    console.log(req)
     console.log(`authorized API request. [action=${req.method} ${req.url}] [params=${JSON.stringify(req.params)}] ` +
     `[query=${JSON.stringify(req.query)}] [body=${JSON.stringify(req.body)}] ` );
 
